9e98a034e7dc5dab2697c8933ac7d56ae903b91b
Klysas <martynasvilkas510@gmail.com>
2015-07-11 18:15:45 +0300

    Created Log class.

---
+++ MyApp/Utils/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSANC.Utils
+{
+	class Log
+	{
+		public static void print(String tag, String message)
+		{
+			Console.WriteLine("{0}: {1}", tag, message);
+		}
+	}
+}
