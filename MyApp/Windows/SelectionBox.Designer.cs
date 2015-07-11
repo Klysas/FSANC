2c62b66174b329728031eac55ce09c9551dc32d2
Klysas <martynasvilkas510@gmail.com>
2015-07-12 00:25:24 +0300

    Added SelectionBox form.

---
+++ MyApp/Windows/SelectionBox.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FSANC.Windows
+{
+	partial class SelectionBox
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Button_Ok = new System.Windows.Forms.Button();
+			this.ListBox_Files = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// Button_Ok
+			// 
+			this.Button_Ok.Enabled = false;
+			this.Button_Ok.Location = new System.Drawing.Point(12, 231);
+			this.Button_Ok.Name = "Button_Ok";
+			this.Button_Ok.Size = new System.Drawing.Size(260, 31);
+			this.Button_Ok.TabIndex = 1;
+			this.Button_Ok.Text = "Ok";
+			this.Button_Ok.UseVisualStyleBackColor = true;
+			this.Button_Ok.Click += new System.EventHandler(this.Button_Ok_Click);
+			// 
+			// ListBox_Files
+			// 
+			this.ListBox_Files.FormattingEnabled = true;
+			this.ListBox_Files.Location = new System.Drawing.Point(12, 12);
+			this.ListBox_Files.Name = "ListBox_Files";
+			this.ListBox_Files.Size = new System.Drawing.Size(260, 212);
+			this.ListBox_Files.TabIndex = 2;
+			this.ListBox_Files.SelectedIndexChanged += new System.EventHandler(this.ListBox_Files_SelectedIndexChanged);
+			// 
+			// SelectionBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 274);
+			this.Controls.Add(this.ListBox_Files);
+			this.Controls.Add(this.Button_Ok);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "SelectionBox";
+			this.ShowIcon = false;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Button_Ok;
+		private System.Windows.Forms.ListBox ListBox_Files;
+	}
+}
---
+++ MyApp/Windows/SelectionBox.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSANC.Windows
+{
+	public partial class SelectionBox : Form
+	{
+		public SelectionBox()
+		{
+			InitializeComponent();
+		}
+
+		#region Private methods
+		private void Button_Ok_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void ListBox_Files_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (ListBox_Files.SelectedIndex != -1)
+			{
+				this.Button_Ok.Enabled = true;
+			}
+		}
+
+		private void updateListBox(VideoFromDatabase[] list)
+		{
+			foreach (VideoFromDatabase video in list)
+			{
+				this.ListBox_Files.Items.Add(video);
+			}
+		}
+
+		private void reset()
+		{
+			this.Button_Ok.Enabled = false;
+			this.ListBox_Files.Items.Clear();
+		}
+
+		#endregion
+
+		#region Public methods
+		public VideoFromDatabase showSelectionBox(VideoFromDatabase[] list)
+		{
+			reset();
+			updateListBox(list);
+			this.ShowDialog();
+
+			return (VideoFromDatabase)this.ListBox_Files.SelectedItem;
+		}
+
+		#endregion
+	}
+}
